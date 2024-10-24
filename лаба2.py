b5a52d238aed9e80c000cc927bbbe54be990dd4c
Вадим Скворцов <skvorcovv83@gmail.com>
2024-10-25 02:58:27 +0700

    Initial commit

---
+++ лаба2.py
@@ -0,0 +1,17 @@
+def main():
+    # Информация о студенте
+    full_name = "Скворцов В.А."
+    faculty = "Информационная безопасность"
+    course = 2
+    group = "ОЗИВ-22у1"
+    email = "skvorcovv83@gmail.com"
+
+    # Вывод информации в консоль
+    print("ФИО студента:", full_name)
+    print("Факультет:", faculty)
+    print("Курс:", course)
+    print("Группа:", group)
+    print("Email:", email)
+
+if __name__ == "__main__":
+    main()
