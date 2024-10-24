761641cf443d3d4b3622c58b1d57729a5cbaf526
Вадим Скворцов <skvorcovv83@gmail.com>
2024-10-25 03:19:12 +0700

    задание 1

---
+++ лаба3задание1.py
@@ -0,0 +1,9 @@
+def zadanie_1():
+    fio = input("Ваши фамилия, имя, отчество? ")
+    hobby = input("Чем Вы увлекаетесь? ")
+
+    print("ФИО:", fio)
+    print("Ваше хобби:", hobby)
+
+
+zadanie_1()
